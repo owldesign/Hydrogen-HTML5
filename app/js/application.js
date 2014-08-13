bcafb1211d66e5f7a175b4208a804604023c90c7
Vadim Goncharov <vadim@owl-design.net>
2014-08-12 18:53:47 -0700

    latest

--- app/js/application.js
+++ app/js/application.js
@@ -1,12 +1,20 @@
 (function() {
-  var loadApplication, mobileNav, mobileScripts;
+  var loadApplication, mobileNav, mobileScripts, ourWorks, svgInjector;
 
   loadApplication = function() {
     var styles;
     styles = ["display: block", "background: #f7cd81", "color: white", "padding: 20px 20px 20px 20px", "text-align: center", "font-weight: normal", "font-size: 20px", "line-height: 60px"].join(';');
     console.log('%c Hydrogen!', styles, 'Has loaded.');
+    $(svgInjector);
     $(mobileScripts);
-    return $(mobileNav);
+    $(mobileNav);
+    return $(ourWorks);
+  };
+
+  svgInjector = function() {
+    var mySVGsToInject;
+    mySVGsToInject = document.querySelectorAll('img.inject-me');
+    return SVGInjector(mySVGsToInject);
   };
 
   mobileScripts = function() {
@@ -16,13 +24,44 @@
   };
 
   mobileNav = function() {
-    var navigation, trigger;
-    trigger = $('.bouncing-arrow');
+    var mobileView, navigation, trigger;
+    trigger = $('.menu-arrow');
     navigation = $('.primary-nav');
-    return trigger.on('click', function() {
+    trigger.on('click', function() {
       navigation.slideToggle();
       navigation.toggleClass('open');
-      return $(this).toggleClass('open');
+      $(this).toggleClass('open');
+      return localStorage.setItem('menu', navigation.hasClass('open'));
+    });
+    mobileView = 767;
+    return window.addEventListener('resize', function() {
+      var flexWidth, menuLs;
+      menuLs = localStorage.getItem('menu');
+      flexWidth = window.innerWidth;
+      if (flexWidth >= mobileView) {
+        if (menuLs === 'false') {
+          navigation.slideDown();
+          navigation.removeClass('open');
+          return trigger.removeClass('open');
+        } else {
+
+        }
+      } else {
+        if (menuLs === 'false') {
+          return navigation.hide();
+        }
+      }
+    });
+  };
+
+  ourWorks = function() {
+    var $container;
+    $container = $('#worksContainer');
+    return $container.isotope({
+      itemSelector: ".item",
+      masonry: {
+        columnWidth: ".grid-sizer"
+      }
     });
   };
 
--- dev/coffee/application.coffee
+++ dev/coffee/application.coffee
@@ -1,19 +1,50 @@
 loadApplication = ->
   styles = ["display: block","background: #f7cd81","color: white","padding: 20px 20px 20px 20px","text-align: center","font-weight: normal","font-size: 20px","line-height: 60px"].join(';')
   console.log '%c Hydrogen!', styles, 'Has loaded.'
+  $(svgInjector)
   $(mobileScripts)
   $(mobileNav)
+  $(ourWorks)
+
+svgInjector = ->
+  mySVGsToInject = document.querySelectorAll('img.inject-me')
+  SVGInjector mySVGsToInject
 
 mobileScripts = ->
   if Modernizr.touch
     FastClick.attach(document.body)
  
 mobileNav = ->
-  trigger = $('.bouncing-arrow')
+  trigger = $('.menu-arrow')
   navigation = $('.primary-nav')
   trigger.on 'click', ->
     navigation.slideToggle()
     navigation.toggleClass 'open'
     $(@).toggleClass 'open'
+    localStorage.setItem 'menu', navigation.hasClass 'open'
+
+  mobileView = 767
+
+  window.addEventListener 'resize', ->
+    menuLs = localStorage.getItem('menu')
+    flexWidth = window.innerWidth
+    
+    if flexWidth >= mobileView
+      if menuLs is 'false'
+        navigation.slideDown()
+        navigation.removeClass 'open'
+        trigger.removeClass 'open'
+      else
+    else
+      if menuLs is 'false'
+        navigation.hide()
+
+ourWorks = ->
+  $container = $('#worksContainer')
+  $container.isotope
+    itemSelector: ".item"
+    masonry:
+      columnWidth: ".grid-sizer"
+
 
 $(loadApplication)
